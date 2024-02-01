96aaf6c9a031299871cc39eb0bba328caaec13f5
binekoo <ghazal.nawel@gmail.com>
2024-02-01 16:32:51 +0100

    fixing commit

---
+++ client-udp/src/main/java/fr/upec/episen/Main.java
@@ -0,0 +1,57 @@
+package fr.upec.episen;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Inet4Address;
+import java.util.Properties;
+import org.apache.logging.log4j.*;
+
+import com.google.common.net.InetAddresses;
+
+public class Main {
+    protected static Logger mainLog = LogManager.getLogger(Main.class);
+    protected static Properties props = new Properties();
+    public static void main(String[] args) {
+        mainLog.info("client-udp started");
+        try{
+            // 0. Chagement du fichier de properties
+            InputStream iStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties");
+            props.load(iStream);
+            final int SIZE = Integer.parseInt(props.getProperty("msg.max.size"));
+
+        String adrValue = "";
+        int portValue = 0;
+        // 1. Traiter les paramètres éventuels sur la ligne de commande
+        if(args.length == 2){ //machine du serveur et port
+            //s'il y en a, les utiliser
+            adrValue = args[0];
+            String portString = args[1];
+            portValue = Integer.parseInt(portString);
+        } else {
+            //utiliser les propriétés par défaut du fichier properties
+            adrValue = props.getProperty("udp.adress");
+            portValue = Integer.parseInt(props.getProperty("udp.port"));
+            }
+
+            // 2. Créer un client
+            Main client = new Main(adrValue, portValue, SIZE);
+
+            // 3.
+            client.run();
+
+        } catch(IOException ioe){
+            mainLog.error((ioe.getMessage()));
+        }
+    }
+
+    protected Integer port;
+    protected Integer packetSize;
+    protected InetAddresses address;
+
+    public Main(final String adrValue, final int portValue, final int size){
+        this.port = portValue;
+        this.packetSize = packetSize;
+        this.address = adrValue;
+    }
+
+}
