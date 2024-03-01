b43e2fea1ffa6362314173dcb738f9c4f412d5a9
Nawel <ghazal.nawel@gmail.com>
2024-03-01 10:16:06 +0100

    Ajout de receveur-tcp

---
+++ TCP/emetteur-tcp/src/main/java/fr/upec/episen/Main.java
@@ -0,0 +1,7 @@
+package fr.upec.episen;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ TCP/receveur-tcp/src/main/java/fr/upec/episen/Main.java
@@ -0,0 +1,34 @@
+package fr.upec.episen;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Properties;
+
+
+public class Main {
+    protected static Logger mainLog = LogManager.getLogger(Main.class);
+    protected static Properties props = new Properties();
+    
+    public static void main(String[] args) {
+        mainLog.warn("receveur-tcp started");
+        //1. Lire les propriétés du projet
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties");
+        try{
+            props.load(is);
+            //2. Créer un serveur TCP
+            final int port = Integer.parseInt(props.getProperty("tcp.port"));
+            ServerSocket ss = new ServerSocket(port);
+            //La socket se créera quand quelque chose sera recç, pas avant.
+            Socket socket = ss.accept();
+            mainLog.warn("one more connection");
+        } catch(IOException ioe){
+            mainLog.error(ioe.getMessage());
+        } finally {
+            mainLog.warn("receveur-tcp stopped");
+        }
+    }
+}
