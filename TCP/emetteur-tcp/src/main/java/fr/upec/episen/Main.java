2596333037d47ec3f65f455c83a6946d960d7780
Nawel <ghazal.nawel@gmail.com>
2024-03-01 10:41:20 +0100

    Ajout d'emetteur TCP

--- TCP/emetteur-tcp/src/main/java/fr/upec/episen/Main.java
+++ TCP/emetteur-tcp/src/main/java/fr/upec/episen/Main.java
@@ -1,7 +1,35 @@
 package fr.upec.episen;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
+    protected static Logger mainLog = LogManager.getLogger(Main.class);
+    protected static Properties props = new Properties();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        mainLog.warn("emetteur-tcp started");
+        //1. Lire les propriétés du projet
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties");
+        try{
+            props.load(is);
+            //2. Créer un serveur TCP
+            final int PORT = Integer.parseInt(props.getProperty("tcp.prot"));
+            final String ADDR = props.getProperty("tcp.address");
+            InetAddress  address = InetAddress.getByName(ADDR);
+            //On se connecte au receveur ici :
+            Socket socket = new Socket(address, PORT);
+            mainLog.warn("emetteur-tcp connected");
+        } catch(IOException ioe){
+            mainLog.error(ioe.getMessage());
+        } finally {
+            mainLog.warn("emetteur tcp stopped");
+        }
     }
 }
--- TCP/receveur-tcp/src/main/java/fr/upec/episen/Main.java
+++ TCP/receveur-tcp/src/main/java/fr/upec/episen/Main.java
@@ -20,11 +20,12 @@ public class Main {
         try{
             props.load(is);
             //2. Créer un serveur TCP
-            final int port = Integer.parseInt(props.getProperty("tcp.port"));
-            ServerSocket ss = new ServerSocket(port);
-            //La socket se créera quand quelque chose sera recç, pas avant.
-            Socket socket = ss.accept();
-            mainLog.warn("one more connection");
+            final int PORT = Integer.parseInt(props.getProperty("tcp.port"));
+            try (ServerSocket ss = new ServerSocket(PORT)){
+                //La socket se créera quand quelque chose sera recç, pas avant.
+                Socket socket = ss.accept();
+                mainLog.warn("one more connection");
+            }
         } catch(IOException ioe){
             mainLog.error(ioe.getMessage());
         } finally {
