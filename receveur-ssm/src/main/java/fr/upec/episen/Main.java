a8e302a4045ecf210babe4bc16585558c9c9070d
Nawel <ghazal.nawel@gmail.com>
2024-03-07 12:02:24 +0100

    génération de logs OK

--- receveur-ssm/src/main/java/fr/upec/episen/Main.java
+++ receveur-ssm/src/main/java/fr/upec/episen/Main.java
@@ -2,7 +2,9 @@ package fr.upec.episen;
 
 import java.io.File;
 import java.io.InputStream;
+import java.text.MessageFormat;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Properties;
 
@@ -22,10 +24,15 @@ public class Main {
             //2. Créer les processus receveur
             mainLog.warn("process creation");
             final int NB_PROCESS = Integer.parseInt(props.getProperty("nb.process"));
-            final String COMMAND = props.getProperty("process.cmd");
+            final String processCmd = props.getProperty("process.cmd");
+            mainLog.warn(System.getProperty("user.dir"));
+            
             for(int index = 0; index < NB_PROCESS; index++){
+                String command = MessageFormat.format(processCmd, Integer.toString(index));
+                String[] commands = command.split(" ");
                 //Création de variables d'environnement pour partager ces infos aux processus générés
-                ProcessBuilder builder = new ProcessBuilder(COMMAND, Integer.toString(index));
+                ProcessBuilder builder = new ProcessBuilder(commands);
+                builder.directory(new File(System.getProperty("user.dir")));
                 Map<String, String> environment = builder.environment();
                 environment.put("UDP_ADRESS", props.getProperty("udp.address"));
                 environment.put("UDP_PORT", props.getProperty("udp.port"));
@@ -34,7 +41,7 @@ public class Main {
                 builder.redirectError(log);
                 Process process = builder.start();
                 mainLog.warn("process num = " + process.pid());
-                Thread.sleep(Duration.ofMinutes(4).toMillis());
+              //  Thread.sleep(Duration.ofMinutes(4).toMillis());
             }
         } catch(Exception e){
             mainLog.error(e.getMessage());
--- receveur-ssm/src/main/java/fr/upec/episen/Receveur.java
+++ receveur-ssm/src/main/java/fr/upec/episen/Receveur.java
@@ -1,5 +1,17 @@
-package fr;
+package fr.upec.episen;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+//Classe principale du process
 public class Receveur {
+        protected static Logger mainLog = LogManager.getLogger(Receveur.class);
+        public static void main(String[] args) {
+            mainLog.warn("Receveur started");
+            final String UDP_ADDR = System.getenv("UDP_ADDRESS");
+            final String UDP_PORT = System.getenv("UDP_PORT");
+            mainLog.warn("adresse : " +  UDP_ADDR);
+            mainLog.warn("port : " + UDP_PORT);
+        }
 
 }
