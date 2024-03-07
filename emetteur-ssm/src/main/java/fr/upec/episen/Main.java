386b575d44b52b18b291d30b0fa84a61a588dfe6
Nawel <ghazal.nawel@gmail.com>
2024-03-07 10:56:56 +0100

    completion classe emetteur-ssm + création classe receveur

--- emetteur-ssm/src/main/java/fr/upec/episen/Main.java
+++ emetteur-ssm/src/main/java/fr/upec/episen/Main.java
@@ -2,15 +2,28 @@ package fr.upec.episen;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.upec.episen.generated.Message;
+
 public class Main {
 
     protected static Logger mainLog = LogManager.getLogger(Main.class);
     protected static Properties props = new Properties();
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
        mainLog.warn("emetteur-ssm started");
         //1. Lire les propriétés du projet
@@ -18,9 +31,30 @@ public class Main {
         try{
             props.load(is);
             //2. Etablir une connexion TCP :
-            final String address = props.getProperty("tcp.address");
-            mainLog.warn("address : " + address);
-        } catch(IOException ioe){
+            final String ADDR = props.getProperty("udp.address");
+            final int PORT = Integer.parseInt(props.getProperty("udp.port"));
+            //Adresse IP pour le groupe.
+            InetAddress group = InetAddress.getByName(ADDR);
+            MulticastSocket socket = new MulticastSocket(PORT);
+            socket.joinGroup(group);
+            //3. Préparation du message à diffuser
+            Message msg = new Message();
+            DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM);
+            msg.setCreation(dateFormat.format(new Date()));
+            msg.setInfo("Encore une heure à attendre la pause");
+            msg.setOrder(1);
+
+            //Convertir le message en binaire
+            ObjectMapper mapper = new ObjectMapper();
+            byte[] content = mapper.writeValueAsBytes(msg);
+            DatagramPacket dp = new DatagramPacket(content, content.length, group,PORT);
+            socket.send(dp);
+            mainLog.warn("Emission de : " + new String(content));
+            Thread.sleep(Duration.ofMinutes(1).toMillis());
+            //4.Fin de la participation au groupe
+            Thread.sleep(Duration.ofMinutes(1).toMillis());
+            socket.leaveGroup(group);
+        } catch(Exception ioe){
             mainLog.error(ioe.getMessage());
         }
     }
---
+++ receveur-ssm/src/main/java/fr/upec/episen/Main.java
@@ -0,0 +1,45 @@
+package fr.upec.episen;
+
+import java.io.File;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+
+    protected static Logger mainLog = LogManager.getLogger(Main.class);
+    protected static Properties props = new Properties();
+    public static void main(String[] args) {
+        mainLog.warn("receveur-ssm started");
+        //1. Lire les propriétés du projet
+        InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties");
+        try{
+            props.load(is);
+            //2. Créer les processus receveur
+            mainLog.warn("process creation");
+            final int NB_PROCESS = Integer.parseInt(props.getProperty("nb.process"));
+            final String COMMAND = props.getProperty("process.cmd");
+            for(int index = 0; index < NB_PROCESS; index++){
+                //Création de variables d'environnement pour partager ces infos aux processus générés
+                ProcessBuilder builder = new ProcessBuilder(COMMAND, Integer.toString(index));
+                Map<String, String> environment = builder.environment();
+                environment.put("UDP_ADRESS", props.getProperty("udp.address"));
+                environment.put("UDP_PORT", props.getProperty("udp.port"));
+                File log = new File("Log" + index);
+                builder.redirectErrorStream(true);
+                builder.redirectError(log);
+                Process process = builder.start();
+                mainLog.warn("process num = " + process.pid());
+                Thread.sleep(Duration.ofMinutes(4).toMillis());
+            }
+        } catch(Exception e){
+            mainLog.error(e.getMessage());
+        } finally{
+            mainLog.warn("receveur-ssm stopped");
+        }
+    }
+}
